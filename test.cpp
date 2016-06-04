ff7c1b989228faaff2d8f267868895838943a1bc
Vladimir Korukov <v_korukov@hotmail.com>
2016-06-04 00:47:18 -0700

    Testing xy 2 neuron character network

--- test.cpp
+++ test.cpp
@@ -4,24 +4,45 @@
 #include <caffe/caffe.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <math.h>
 
 using namespace std;
 using namespace caffe;
 using namespace cv;
 
+#define RANDDOUBLE ((rand() % RAND_MAX) * 1.0 / RAND_MAX)
+#define BK_GROUND -1.0f
+#define FR_GROUND 1.0f
+
 void renderRect(int imW, int imH, int rW, int rH, int cx, int cy, float rot, float* data) {
-    float bkGround = -1;
-    float col = 1;
     float c = cos(rot);
     float s = sin(rot);
     for (int y = 0; y < imH; y++) {
         for (int x = 0; x < imW; x++) {
-            float locx = (x - cx) * c - (y - cy) * s;
-            float locy = (x - cx) * s + (y - cy) * c;
-            data[x + imW * y] = (-rW / 2 < locx) && (locx < rW / 2) && (-rH / 2 < locy) && (locy < rH / 2) ? col : bkGround;
+            int locx = (x - cx) * c - (y - cy) * s;
+            int locy = (x - cx) * s + (y - cy) * c;
+            data[x + imW * y] = (-rW / 2 < locx) && (locx < rW / 2) && (-rH / 2 < locy) && (locy < rH / 2) ? FR_GROUND : BK_GROUND;
         }
     }
+}
+
+void renderImage(int imW, int imH, int cx, int cy, float rot, float* data, const Mat& img) {
+    warpAffine(img, Mat(imH, imW, CV_32F, data),
+        (Mat_<float>(2, 3) << 1, 0, cx, 0, 1, cy) * // translate to desired location
+        (Mat_<float>(3, 3) << cos(rot), -sin(rot), 0, sin(rot), cos(rot), 0, 0, 0, 1) * // rotate
+        (Mat_<float>(3, 3) << 1, 0, -img.rows / 2, 0, 1, -img.cols / 2, 0, 0, 1), // center
+        Size(imH, imW), INTER_LINEAR, BORDER_CONSTANT, Scalar(BK_GROUND));
+}
+
+void drawDat(const char* fname, int w, int h, float* dat, function<float(float)> f) {
+    Mat m(h, w, CV_32F);
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            m.at<float>(y, x) = f(dat[y * w + x]);
+        }
+    }
+    imwrite(fname, m);
 }
 
 int main(int argc, char ** argv) {
@@ -29,6 +50,13 @@ int main(int argc, char ** argv) {
     // renderRect(100, 100, 30, 30, 70, 50, 0.4, (float*)rm.data);
     // imwrite("test.png", rm);
     // return 0;
+    Mat characterImg = imread("../character.png", CV_LOAD_IMAGE_GRAYSCALE);
+    Mat character(50, 50, CV_32F);
+    for (int j = 0; j < characterImg.cols; j++) {
+        for (int i = 0; i < characterImg.rows; i++) {
+            character.at<float>(i, j) = characterImg.at<unsigned char>(i, j) / -255.0 * (FR_GROUND - BK_GROUND) + FR_GROUND;
+        }
+    }
 
     srand(time(0));
     ::google::InitGoogleLogging(argv[0]);
@@ -53,51 +81,24 @@ int main(int argc, char ** argv) {
     // float labPtr = labels + i * NUM_LABELS;
 
     // Try generate random params:
-    float rot = (rand() % 10000) * 1.0 / 10000.0 * M_PI / 2;
+    float rot = RANDDOUBLE * M_PI * 2;
     int randx = rand() % 30 + 35;
     int randy = rand() % 30 + 35;
     int randdx = rand() % 11 - 5;
     int randdy = rand() % 11 - 5;
-    float drot = 0;//(rand() % 10000) * 1.0 / 10000.0 * M_PI / 5 - M_PI / 10;
+    float drot = 0;//RANDDOUBLE * M_PI / 5 - M_PI / 10;
     float c = cos(rot);
     float s = sin(rot);
     float cp = cos(rot + drot);
     float sp = sin(rot + drot);
-    // int locx = (50 - randx) * cos(rot) - (50 - randy) * sin(rot);
-    // int locy = (50 - randx) * sin(rot) + (50 - randy) * cos(rot);
-    // int movx = locx * cos(rot + drot) + locy * sin(rot + drot) + randx + randdx - 50;
-    // int movy = -locx * sin(rot + drot) + locy * cos(rot + drot) + randy + randdy - 50;
-    renderRect(WIDTH, HEIGHT, rectW, rectH, randx, randy, rot, img1Ptr);
-    renderRect(WIDTH, HEIGHT, rectW, rectH, randx + randdx, randy + randdy, rot + drot, img2Ptr);
-    // check that center is 1
-    // while ((imgPtr[WIDTH / 2 + WIDTH * (HEIGHT / 2)] != 1) || ((movx > 5) || (movx < -5) || (movy > 5) || (movy < -5))) {
-    //     rot = (rand() % 10000) * 1.0 / 10000.0 * M_PI / 2;
-    //     randx = rand() % 30 + 35;
-    //     randy = rand() % 30 + 35;
-    //     randdx = rand() % 11 - 5;
-    //     randdy = rand() % 11 - 5;
-    //     drot = 0;//(rand() % 10000) * 1.0 / 10000.0 * M_PI / 5 - M_PI / 10;
-    //     locx = (50 - randx) * cos(rot) - (50 - randy) * sin(rot);
-    //     locy = (50 - randx) * sin(rot) + (50 - randy) * cos(rot);
-    //     movx = locx * cos(rot + drot) + locy * sin(rot + drot) + randx + randdx - 50;
-    //     movy = -locx * sin(rot + drot) + locy * cos(rot + drot) + randy + randdy - 50;
-    //     renderRect(WIDTH, HEIGHT, rectW, rectH, randx, randy, rot, img1Ptr);
-    //     renderRect(WIDTH, HEIGHT, rectW, rectH, randx + randdx, randy + randdy, rot + drot, img2Ptr);
-    //     cout << "." << endl;
-    // }
 
+    renderImage(100, 100, randx, randy, rot, img1Ptr, character);
+    renderImage(100, 100, randx + randdx, randy + randdy, rot + drot, img2Ptr, character);
+    // renderRect(WIDTH, HEIGHT, rectW, rectH, randx, randy, rot, img1Ptr);
+    // renderRect(WIDTH, HEIGHT, rectW, rectH, randx + randdx, randy + randdy, rot + drot, img2Ptr);
 
-
-    Mat rm1(100, 100, CV_32F);
-    Mat rm2(100, 100, CV_32F);
-    for (int x = 0; x < 100; x++) {
-       for (int y = 0; y < 100; y++) {
-           rm1.at<float>(x, y) = img1Ptr[x + 100 * y] == 1 ? 255 : 0;
-           rm2.at<float>(x, y) = img2Ptr[x + 100 * y] == 1 ? 255 : 0;
-       }
-    }
-    imwrite("dmap1.png", rm1);
-    imwrite("dmap2.png", rm2);
+    drawDat("dmap1.png", 100, 100, img1Ptr, [](float x) { return x * 127 + 128; });
+    drawDat("dmap2.png", 100, 100, img2Ptr, [](float x) { return x * 127 + 128; });
 
     cv::Mat src(100, 100, CV_8UC3, Scalar::all(0)),
             act(100, 100, CV_8UC3, Scalar::all(0)),
@@ -105,11 +106,12 @@ int main(int argc, char ** argv) {
 
     int curI = 0;
     int usedColors = 0;
-    int testParity = 0;
+    int totGuesses = 0;
+    int totCorrect = 0;
     while (true) {
         vector<pair<int, int>> loadedNums;
         vector<Vec3b> loadedColors;
-        vector<int> gtGuess;
+        vector<pair<int, int>> gtGuess;
         for (int i = 0; i < BATCH_SIZE; i++) {
             int x = curI / 100;
             int y = curI % 100;
@@ -124,28 +126,24 @@ int main(int argc, char ** argv) {
                 break;
             Vec3b col(127.5 * (1 + sin(usedColors / 100.0)), 127.5 * (1 + cos(usedColors / 100.0)), 127.5 * (1 + sin(usedColors / 200.0) + sin(usedColors / 13.0)));
             usedColors++;
-            src.at<Vec3b>(x, y) = col;
+            src.at<Vec3b>(y, x) = col;
 
-            float locx = (x - randx) * c - (y - randy) * s;
-            float locy = (x - randx) * s + (y - randy) * c;
+            float locx = (x - randx) * c + (y - randy) * s;
+            float locy = -(x - randx) * s + (y - randy) * c;
 
-            int actx = (locx * cp + locy * sp) + (randx + randdx);
-            int acty = (-locx * sp + locy * cp) + (randy + randdy);
+            int actx = (locx * cp - locy * sp) + (randx + randdx);
+            int acty = (locx * sp + locy * cp) + (randy + randdy);
+            int movx = actx - x;
+            int movy = acty - y;
 
-            act.at<Vec3b>(actx, acty) = col;
+            act.at<Vec3b>(acty, actx) = col;
 
             loadedColors.push_back(col);
             loadedNums.push_back(make_pair(x, y));
-            gtGuess.push_back(((actx - x) + 5) + ((acty - y) + 5) * 11);
+            gtGuess.push_back(make_pair(movx, movy));
 
-            // int preddx = (actx - x);
-            // int preddy = (acty - y);
-            // if (testParity % 8 < 4)
-            //     preddx *= -1;
-            // if (testParity % 4 < 2)
-            //     preddy *= -1;
-            // net->input_blobs()[1]->mutable_cpu_data()[i] = (testParity % 2) ? (preddx + 5) + (preddy + 5) * 11 : (preddy + 5) + (preddx + 5) * 11;
-            net->input_blobs()[1]->mutable_cpu_data()[i] = ((actx - x) + 5) + ((acty - y) + 5) * 11;
+            net->input_blobs()[1]->mutable_cpu_data()[2 * i] = movx;
+            net->input_blobs()[1]->mutable_cpu_data()[2 * i + 1] = movy;
             float* curData = net->input_blobs()[0]->mutable_cpu_data() + 100 * 100 * 2 * i;
             for (int pry = 0; pry < 100; pry++) {
                 for (int prx = 0; prx < 100; prx++) {
@@ -157,7 +155,7 @@ int main(int argc, char ** argv) {
                     } else {
                         curData[prx + 100 * pry] = img1Ptr[cx + 100 * cy];
                         curData[prx + 100 * pry + 100 * 100] = img2Ptr[cx + 100 * cy];
-                    }       
+                    }
                 }
             }
 
@@ -165,31 +163,27 @@ int main(int argc, char ** argv) {
             if (curI == 10000)
                 break;
         }
-        // cout << (testParity % 2 ? "x then y " : "y then x ") << (testParity % 4 < 2 ? "-y " : "+y ") << (testParity % 8 < 4 ? "-x " : "+x ") << endl;
-        // float* labss = (float*)malloc(BATCH_SIZE * sizeof(float) * NUM_LABELS);
-        // memcpy(labss, labels, BATCH_SIZE * sizeof(float) * NUM_LABELS);
-        // net->input_blobs()[1]->set_cpu_data(labss);
-        testParity++;
         float loss;
         net->Forward(&loss);
-        cout << "loss: " << loss << endl;
-        const float* guesses = net->blob_by_name("fc8")->cpu_data();
+        // cout << "loss: " << loss << endl;
+        const float* guesses_x = net->blob_by_name("fc8_x")->cpu_data();
+        const float* guesses_y = net->blob_by_name("fc8_y")->cpu_data();
         for (int i = 0; i < loadedNums.size(); i++) {
-            int maxGuess = 0;
-            for (int j = 1; j < 121; j++) {
-                if (guesses[maxGuess] < guesses[j])
-                    maxGuess = j;
-            }
-            // maxGuess = gtGuess[i];
-            int guessx = maxGuess % 11 - 5;
-            int guessy = maxGuess / 11 - 5;
-            pred.at<Vec3b>(loadedNums[i].first + guessx, loadedNums[i].second + guessy) = loadedColors[i];
-            guesses += 121;
+            int guessx = guesses_x[0];
+            int guessy = guesses_y[0];
+            totGuesses++;
+            if ((abs(guessx - gtGuess[i].first) <= 1) && (abs(guessy - gtGuess[i].second) <= 1))
+                totCorrect++;
+            pred.at<Vec3b>(loadedNums[i].second + guessy, loadedNums[i].first + guessx) = loadedColors[i];
+            guesses_x++;
+            guesses_y++;
         }
 
         if (curI == 10000)
             break;
     }
+
+    cout << "Correcness: " << totCorrect << "/" << totGuesses << endl;
 
     imwrite("src.png", src);
     imwrite("act.png", act);
